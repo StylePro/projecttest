a15b1d819610ffa41b423350e5afaa65cd42dc02
StylePro <sergeev.gold@gmail.com>
2023-02-10 21:59:38 +0300

    store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import state, {addPost, sendMessage, subscriber, updateMessageBody, updatePostText} from "./state/state";
+import store from "./state/state";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,13 +11,13 @@ const rerenderEntireTree = (state)=> {
     root.render(<React.StrictMode>
         <BrowserRouter>
             <App state={state}
-                 addPost={addPost}
-                 updatePostText={updatePostText}
-                 sendMessage={sendMessage}
-                 updateMessageBody={updateMessageBody}/>
+                 addPost={store.addPost.bind(store)}
+                 updatePostText={store.updatePostText.bind(store)}
+                 sendMessage={store.sendMessage.bind(store)}
+                 updateMessageBody={store.updateMessageBody.bind(store)}/>
         </BrowserRouter>
     </React.StrictMode>);
 }
 
-rerenderEntireTree(state);
-subscriber(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscriber(rerenderEntireTree);
--- src/state/state.jsx
+++ src/state/state.jsx
@@ -1,65 +1,67 @@
-let rerenderEntireTree = ()=> {
-    console.log('state new')
-}
+const store = {
 
-const state = {
-    profilePage: {
-        profile: [
-            {id: 1, message: 'Hello', likesCount: 89},
-            {id: 2, message: 'What do you do', likesCount: 34},
-            {id: 3, message: 'What is your name', likesCount: 23},
-            {id: 4, message: 'All good', likesCount: 45},
-            {id: 5, message: 'Ogogo', likesCount: 43},
-        ],
-        newPostText: '',
+    _rerenderEntireTree() {
+        console.log('state new')
     },
-    dialogPage: {
-        dialogs: [
-            {id: 1, name: 'Oleg', surname: 'Sergeev'},
-            {id: 2, name: 'Viktor', surname: 'Braga'},
-            {id: 3, name: 'Egor', surname: 'Dydkin'},
-            {id: 4, name: 'Alex', surname: 'Drag'},
-        ],
-        message: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'Hello'},
-            {id: 3, message: 'Who'},
-            {id: 4, message: 'Ho Ho Ho'},
-        ],
-        newMessageBody: '',
+    _state: {
+        profilePage: {
+            profile: [
+                {id: 1, message: 'Hello', likesCount: 89},
+                {id: 2, message: 'What do you do', likesCount: 34},
+                {id: 3, message: 'What is your name', likesCount: 23},
+                {id: 4, message: 'All good', likesCount: 45},
+                {id: 5, message: 'Ogogo', likesCount: 43},
+            ],
+            newPostText: '',
+        },
+        dialogPage: {
+            dialogs: [
+                {id: 1, name: 'Oleg', surname: 'Sergeev'},
+                {id: 2, name: 'Viktor', surname: 'Braga'},
+                {id: 3, name: 'Egor', surname: 'Dydkin'},
+                {id: 4, name: 'Alex', surname: 'Drag'},
+            ],
+            message: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Hello'},
+                {id: 3, message: 'Who'},
+                {id: 4, message: 'Ho Ho Ho'},
+            ],
+            newMessageBody: '',
+        },
     },
+    getState() {
+        return this._state
+    },
+    addPost() {
+        const newPost = {
+            id: Date.now(),
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        }
+        this._state.profilePage.profile.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._rerenderEntireTree(this._state);
+    },
+    updatePostText(text) {
+        this._state.profilePage.newPostText = text;
+        this._rerenderEntireTree(this._state);
+    },
+    sendMessage () {
+        let newMessage = {
+            id: Date.now(),
+            message: this._state.dialogPage.newMessageBody
+        }
+        this._state.dialogPage.message.push(newMessage);
+        this._state.dialogPage.newMessageBody = '';
+        this._rerenderEntireTree(this._state);
+    },
+    updateMessageBody(messageText) {
+        this._state.dialogPage.newMessageBody = messageText;
+        this._rerenderEntireTree(this._state);
+    },
+    subscriber(observer) {
+        this._rerenderEntireTree = observer;
 }
-
-export const addPost = () => {
-    const newPost = {
-        id: Date.now(),
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    }
-    state.profilePage.profile.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
-export const updatePostText = (text) => {
-    state.profilePage.newPostText = text;
-    rerenderEntireTree(state);
-}
-export const sendMessage = () => {
-    let newMessage = {
-        id: Date.now(),
-        message: state.dialogPage.newMessageBody
-    }
-    state.dialogPage.message.push(newMessage);
-    state.dialogPage.newMessageBody = '';
-    rerenderEntireTree(state);
-}
-export const updateMessageBody = (messageText) => {
-    state.dialogPage.newMessageBody = messageText;
-    rerenderEntireTree(state);
-}
-
-export const subscriber = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
