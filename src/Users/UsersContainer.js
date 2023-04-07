f3260a91716049993c3d7f3666142d047acea3cd
style-pro <sergeev.gold@gmail.com>
2023-04-07 12:32:18 +0300

    Lesson 54

--- src/Users/UsersContainer.js
+++ src/Users/UsersContainer.js
@@ -5,8 +5,6 @@ import {followAC, setUsersAC, unfollowAC} from "../redux/users-reducer";
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,  /*Размер страницы*/
-        totalUsersCount: state.usersPage.totalUsersCount /*Всего страниц*/
     }
 }
 let mapDispatchToProps = (dispatch) => {
